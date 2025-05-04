2b5b6270297d3b5c274a4f26bfbcaaf60ffd9bb5
Luisma9530 <luismasolares@gmail.com>
2025-05-04 06:38:23 +0200

    Updated socket

--- src/socket.ts
+++ src/socket.ts
@@ -1,6 +1,11 @@
 import { io } from "socket.io-client";
 
-const socket = io(`${window.location.hostname}:4000`, {
+const URL =
+  process.env.NODE_ENV === "production"
+    ? "https://cazahadas.onrender.com" // reemplaza con tu dominio real
+    : "http://localhost:4000";
+
+const socket = io(URL, {
   autoConnect: false,
 });
 
